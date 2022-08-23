de1c1fb45280308dd34b1b00d80b754fbbf69407
Hans Zizold <hzizold@hotmail.com>
2022-08-23 16:07:30 -0400

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,3 +1,22 @@
 /*
     Create a database based on a diagram
 */
+CREATE DATABASE clinic;
+
+-- Create patient table
+
+CREATE TABLE patients(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name varchar(250),
+  date_of_birth date
+);
+
+-- Create medical histories
+
+CREATE TABLE medical_histories(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  admitted_at date,
+  patient_id INT FOREIGN KEY REFERENCES patients(id),
+  status varchar(100)
+);
+
