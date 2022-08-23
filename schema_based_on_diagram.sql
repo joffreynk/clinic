3ebf624ba8b75222fec86b214ecd3b401186b91a
Hans Zizold <hzizold@hotmail.com>
2022-08-23 18:36:00 -0400

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,21 +3,14 @@
 */
 CREATE DATABASE clinic;
 
+\c clinic;
+
 -- Create patient table
 
 CREATE TABLE patients(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name varchar(250),
   date_of_birth date
-);
-
--- Create medical histories
-
-CREATE TABLE medical_histories(
-  id INT PRIMARY KEY FOREIGN KEY REFERENCES treatments(id),
-  admitted_at date,
-  patient_id INT FOREIGN KEY REFERENCES patients(id),
-  status varchar(100)
 );
 
 --  create treatments table
@@ -28,6 +21,27 @@ CREATE TABLE treatments(
   name varchar(200)
 );
 
+-- Create medical histories
+
+CREATE TABLE medical_histories(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  admitted_at date,
+  status varchar(100),
+  patient_id INT,
+  CONSTRAINT patientid_ref FOREIGN KEY(patient_id) REFERENCES patients(id)
+);
+
+--  create invoices table
+
+CREATE TABLE invoices(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  total_amount decimal,
+  generated_at date,
+  payed_at date,
+  medical_history_id INT,
+  CONSTRAINT medical_histories_ref FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id)
+);
+
 --  create invoice items table
 
 CREATE TABLE invoice_items(
@@ -35,6 +49,19 @@ CREATE TABLE invoice_items(
   unit_price decimal,
   quantity INT,
   total_price decimal,
-  invoice_id INT FOREIGN KEY REFERENCES invoices(id),
-  treatment_id INT FOREIGN KEY REFERENCES treatments(id)
+  invoice_id INT,
+  treatment_id INT,
+  CONSTRAINT invoices_ref FOREIGN KEY(invoice_id) REFERENCES invoices(id),
+  CONSTRAINT treatments_ref FOREIGN KEY(treatment_id) REFERENCES treatments(id)
 );
+
+-- create an intermmediate table for medical_history and treatments
+
+CREATE TABLE treatments_medical_history(
+  medical_history_id INT,
+  treatment_id INT,
+  CONSTRAINT medical_history_ref FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id),
+  CONSTRAINT treatment_ref FOREIGN KEY(treatment_id) REFERENCES treatments(id),
+  PRIMARY KEY (medical_history_id, treatment_id)
+);
+
