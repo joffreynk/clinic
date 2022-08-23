37e7859f0c88577d4764114cc8ff2268c2281034
Joffreynk <joyjoffrey@gmail.com>
2022-08-23 22:25:01 +0200

    implement treatment and invoices items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,9 +14,27 @@ CREATE TABLE patients(
 -- Create medical histories
 
 CREATE TABLE medical_histories(
-  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  id INT PRIMARY KEY FOREIGN KEY REFERENCES treatments(id),
   admitted_at date,
   patient_id INT FOREIGN KEY REFERENCES patients(id),
   status varchar(100)
 );
 
+--  create treatments table
+
+CREATE TABLE treatments(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  type varchar(250),
+  name varchar(200)
+);
+
+--  create invoice items table
+
+CREATE TABLE invoice_items(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  unit_price decimal,
+  quantity INT,
+  total_price decimal,
+  invoice_id INT FOREIGN KEY REFERENCES invoices(id),
+  treatment_id INT FOREIGN KEY REFERENCES treatments(id)
+);
